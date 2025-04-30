08d4842a8cf0dcfe70e14139e1fc3bb53a5554f0
Michael Wytock <mwytock@stride.build>
2025-04-30 15:48:56 -0400

    Fix connection string blocking build

--- database.vb
+++ database.vb
@@ -1,7 +1,7 @@
 ﻿Imports System.Data.SqlClient
 
 Module database
-    Public sqlcon As New SqlConnection("server = "YOUR_SERVER_NAME"; database = lab3; Integrated security = True")
+    Public sqlcon As New SqlConnection("server = 'YOUR_SERVER_NAME'; database = lab3; Integrated security = True")
     Public Function GetSQLConnection() As SqlConnection
         Return sqlcon
     End Function
